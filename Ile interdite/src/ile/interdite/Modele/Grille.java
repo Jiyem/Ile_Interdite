5624d25a99e9d8f9c98eaa927b29e16281dde816
miras <miras@miras-HP-ProBook-640-G1>
2018-12-06 20:42:55 +0100

    mélange et manipulation grille

--- Ile interdite/src/ile/interdite/Modele/Grille.java
+++ Ile interdite/src/ile/interdite/Modele/Grille.java
@@ -6,50 +6,64 @@
 package ile.interdite.Modele;
 
 import java.util.ArrayList;
+import java.util.Collections;
 
 /**
  *
  * @author rose
  */
 public class Grille {
+
     //chercher quel type de colection pour les 36 tuiles de la grille
     private final Tuile tuile[][] = new Tuile[6][6];
-    private ArrayList<String> tuilesUtilisé = new ArrayList(); 
+    private ArrayList<String> tuilesUtilisé = new ArrayList();
     private ArrayList<String> tuilesDispo = new ArrayList();
     private ListeTuiles liste;
-    public Grille(){
+
+    public Grille() {
+
+    }
+
+    public void initialiseArray() {
+        for (int i = 0; i < 24; i++) {
+            this.tuilesDispo.add(i, liste.toString());
+        }
+
+    }
+
+    public Grille(ArrayList tuiles) {
+        this.initialiseArray();
+        int nb = 0;
+        for (int x = 0; x < 5; x++) {
+            for (int y = 0; y < 5; y++) {
+
+                if ((x == 0 && y == 0) || (x == 0 && y == 1) || (x == 0 && y == 4) || (x == 0 && y == 5) || (x == 1 && y == 0) || (x == 1 && y == 5) || (x == 4 && y == 0) || (x == 4 && y == 5) || (x == 5 && y == 0) || (x == 5 && y == 1) || (x == 5 && y == 4) || (x == 5 && y == 5)) {
+
+                } else {
+                    nb = nb + 1;
+                    this.tuile[x][y] = new Tuile(nb, tuilesDispo.get(0));
+                    this.tuilesUtilisé.add(tuilesDispo.get(nb));
+                    this.tuilesDispo.remove(tuilesDispo.get(nb));
+
+                }
+            }
+
+        }
+
+    }
+
+    
+    
+
+    public void remetTuileDispo(){
+    for (int i=0;i<24;i++){
+        this.tuilesDispo.add(tuilesUtilisé.get(i));
+        this.tuilesUtilisé.remove(tuilesUtilisé.get(i));
         
     }
     
-    public void initialiseArray(){
-        for (int i =0; i<24; i++){
-            this.tuilesDispo.add(i, liste.toString());
-        }
-        
     }
-    public Grille(ArrayList tuiles){
-        this.initialiseArray();
-        int nb =0;
-        for (int x=0;  x< 5; x++  ){
-            for(int y=0; y<5; y++){
-                
-                if ((x==0 && y==0) ||(x==0 && y==1)||(x==0 && y==4)||(x==0 && y==5)||(x==1 && y==0)||(x==1 && y==5)||(x==4 && y==0)||(x==4 && y==5)||(x==5 && y==0)||(x==5 && y==1)||(x==5 && y==4)||(x==5 && y==5) ){
-                    
-                } 
-                else {
-                nb = nb+1;    
-                this.tuile[x][y] =new Tuile(nb,tuilesDispo.get(0));
-                this.tuilesUtilisé.add(tuilesDispo.get(nb));
-                this.tuilesDispo.remove(tuilesDispo.get(nb));
-                
-                }
-            }
-        
-    }
- 
-    }
-    public void Shuffle(){
-        this.tuilesDispo.Shuffle
-        
+    public void melange() {
+        Collections.shuffle(tuilesDispo);
     }
 }
