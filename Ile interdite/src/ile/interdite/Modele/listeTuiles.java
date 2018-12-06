0fe5e19fb53b17f4736b6ec8c20ea63b3176a9d9
mirasl <mirasl@pc-dg-025-11.iut2.upmf-grenoble.fr>
2018-12-06 09:14:15 +0100

    Liste Tuiles

---
+++ Ile interdite/src/ile/interdite/Modele/listeTuiles.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ile.interdite.Modele;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mirasl
+ */
+public class listeTuiles {
+    private ArrayList nomTuile = new ArrayList();
+    
+    public listeTuiles(){
+        
+     nomTuile.add("Le Pont des Abimes");
+     nomTuile.add("La Porte de Bronze");
+     nomTuile.add("La Caverne des Ombres");
+     nomTuile.add("La Porte de Fer"); 
+     nomTuile.add("La Porte d’Or");
+     nomTuile.add("Les Falaises de l’Oubli");
+     nomTuile.add("Le Palais de Corail");
+     nomTuile.add("La Porte d’Argent");
+     nomTuile.add("Les Dunes de l’Illusion");
+     nomTuile.add("Heliport"); 
+     nomTuile.add("La Porte de Cuivre");
+     nomTuile.add("Le Jardin des Hurlements");
+     nomTuile.add("La Foret Pourpre");
+     nomTuile.add("Le Lagon Perdu");
+     nomTuile.add("Le Marais Brumeux");
+     nomTuile.add("Observatoire");
+     nomTuile.add("Le Rocher Fantome");
+     nomTuile.add("La Caverne du Brasier");
+     nomTuile.add("Le Temple du Soleil");
+     nomTuile.add("Le Temple de La Lune");
+     nomTuile.add("Le Palais des Marees");
+     nomTuile.add("Le Val du Crepuscule");
+     nomTuile.add("La Tour du Guet");
+     nomTuile.add("Le Jardin des Murmures");
+    }
+    
+}
