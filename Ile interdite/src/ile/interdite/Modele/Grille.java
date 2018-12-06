1c8368b6553c7a5b592a6fbbda345a29f3c9b905
mirasl <mirasl@pc-dg-025-11.iut2.upmf-grenoble.fr>
2018-12-06 09:46:20 +0100

    Création grille

--- Ile interdite/src/ile/interdite/Modele/Grille.java
+++ Ile interdite/src/ile/interdite/Modele/Grille.java
@@ -14,15 +14,35 @@ import java.util.ArrayList;
 public class Grille {
     //chercher quel type de colection pour les 36 tuiles de la grille
     private final Tuile tuile[][] = new Tuile[6][6];
-    private ArrayList tuilesUtilisé = new ArrayList(); 
-    private listeTuiles tuiles = new listeTuiles();
+    private ArrayList<String> tuilesUtilisé = new ArrayList(); 
+    private ArrayList<String> tuilesDispo = new ArrayList();
+    private ListeTuiles liste;
     public Grille(){
         
     }
     
     public void initialiseArray(){
+        for (int i =0; i<24; i++){
+            this.tuilesDispo.add(i, liste.toString());
+        }
         
-        this.tuiles.addAll();
     }
-    
+    public Grille(ArrayList tuiles){
+        this.initialiseArray();
+        for (int x=0;  x< 5; x++  ){
+            for(int y=0; y<5; y++){
+                int nb = x*y;
+                if ((x==0 && y==0) ||(x==0 && y==1)||(x==0 && y==4)||(x==0 && y==5)||(x==1 && y==0)||(x==1 && y==5)||(x==4 && y==0)||(x==4 && y==5)||(x==5 && y==0)||(x==5 && y==1)||(x==5 && y==4)||(x==5 && y==5) ){
+                    
+                } 
+                else {                
+                this.tuile[x][y] =new Tuile(nb,tuilesDispo.get(nb));
+                this.tuilesUtilisé.add(tuilesDispo.get(nb));
+                this.tuilesDispo.remove(tuilesDispo.get(nb));
+                
+                }
+            }
+        
+    }
+    }
 }
---
+++ Ile interdite/src/ile/interdite/Modele/ListeTuiles.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ile.interdite.Modele;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author mirasl
+ */
+public class ListeTuiles {
+    private final String[] nom = {"Le Pont des Abimes",
+    "La Porte de Bronze",
+    "La Caverne des Ombres",
+    "La Porte de Fer", 
+    "La Porte d’Or", 
+    "Les Falaises de l’Oubli",
+    "Le Palais de Corail",
+    "La Porte d’Argent", 
+    "Les Dunes de l’Illusion",
+    "Heliport", 
+    "La Porte de Cuivre",
+    "Le Jardin des Hurlements",
+    "La Foret Pourpre",
+    "Le Lagon Perdu",
+    "Le Marais Brumeux",
+    "Observatoire",
+    "Le Rocher Fantome",
+    "La Caverne du Brasier",
+    "Le Temple du Soleil",
+    "Le Temple de La Lune",
+    "Le Palais des Marees",
+    "Le Val du Crepuscule",
+    "La Tour du Guet",
+    "Le Jardin des Murmures"};
+    
+    public ListeTuiles(){
+        
+     
+    }
+    
+}
