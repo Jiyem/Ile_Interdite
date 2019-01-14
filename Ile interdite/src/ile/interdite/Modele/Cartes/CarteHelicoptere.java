01e078c88174186d733f72bae3984a6bbb66495e
Jiyem <Jiyem@192.168.0.18>
2019-01-14 10:25:12 +0100

    Actions des cartes Hélico  + Sac de sable.

--- Ile interdite/src/ile/interdite/Modele/Cartes/CarteHelicoptere.java
+++ Ile interdite/src/ile/interdite/Modele/Cartes/CarteHelicoptere.java
@@ -5,6 +5,13 @@
  */
 package ile.interdite.Modele.Cartes;
 
+import ile.interdite.Modele.Aventuriers.Aventurier;
+import static ile.interdite.Modele.EtatCase.INNONDEE;
+import static ile.interdite.Modele.EtatCase.NORMAL;
+import ile.interdite.Modele.Grille;
+import ile.interdite.Modele.Tuile;
+import java.util.ArrayList;
+
 /**
  *
  * @author anandanj
@@ -13,4 +20,39 @@ public class CarteHelicoptere extends CarteTirage{
     public CarteHelicoptere(){
         super(TypeCarte.Helicoptere);
     }
+    
+    //Méthode qui renvoie toutes les tuiles (ArrayList) sur lesquels les joueurs peuvent s'héliporter.
+    public ArrayList<Tuile> deplacable(Grille grille){
+        ArrayList<Tuile> tuilesDeplacable = new ArrayList<>();
+        Tuile[][] tuiles = grille.getTuile();
+        for (int y = 0; y < 6; y++) {
+            for (int x = 0; x < 6; x++) {
+                if(tuiles[y][x].getEtatCase()== INNONDEE ||tuiles[y][x].getEtatCase() == NORMAL){
+                    tuilesDeplacable.add(tuiles[y][x]);
+                }
+            }
+        }
+        return tuilesDeplacable;
+    }
+    
+    //Méthode qui demande la tuile de départ utilisé par la carte, la tuile d'arrivé et l'ArrayList des joueurs qui veulent être déplacé.
+    //  /!\ on part du principe que la méthode deplacable à été utilisé au préalable et que tuileDep et tuileArr sont bien dans les tuiles possible.
+    //La méthode vérifie que tout les joueurs sont sur la tuile de départ sinon lève une exception (a supprimer plus tard en fonction de comment on code dans le contrôleur)
+    //Une fois la vérification éffectuée la méthode déplace les joueurs sur la tuile d'arrivé.
+    public void utiliserHelico(Tuile tuileDep,Tuile tuileArr,ArrayList<Aventurier> joueurs){
+        boolean verifJoueursSurTuile = true;
+        for(int i = 0;i<joueurs.size();i++){
+            if(joueurs.get(i).getPosition() != tuileDep){
+                verifJoueursSurTuile = false;
+            }
+        }
+        if(!verifJoueursSurTuile){
+            //faudra lever une exception
+        }else{
+            
+        }for(int i = 0;i<joueurs.size();i++){
+            joueurs.get(i).deplacement(tuileArr);
+        }
+    }
+    
 }
--- Ile interdite/src/ile/interdite/Modele/Cartes/CarteSacDeSable.java
+++ Ile interdite/src/ile/interdite/Modele/Cartes/CarteSacDeSable.java
@@ -5,6 +5,12 @@
  */
 package ile.interdite.Modele.Cartes;
 
+import static ile.interdite.Modele.EtatCase.INNONDEE;
+import static ile.interdite.Modele.EtatCase.NORMAL;
+import ile.interdite.Modele.Grille;
+import ile.interdite.Modele.Tuile;
+import java.util.ArrayList;
+
 /**
  *
  * @author anandanj
@@ -13,4 +19,27 @@ public class CarteSacDeSable extends CarteTirage {
     public CarteSacDeSable(){
         super(TypeCarte.SacDeSable);
     }
+    
+    //Méthode qui renvoie la liste des tuiles qui sont assèchable avec un sac de sable avec pour paramètre d'entrée une grille.
+    public ArrayList<Tuile> assechable(Grille grille){
+        ArrayList<Tuile> tuilesAssechable = new ArrayList<>();
+        Tuile[][] tuiles = grille.getTuile();
+        for (int y = 0; y < 6; y++) {
+            for (int x = 0; x < 6; x++) {
+                if(tuiles[y][x].getEtatCase()==INNONDEE){
+                    tuilesAssechable.add(tuiles[y][x]);
+                }
+            }
+        }
+        return tuilesAssechable;
+    }
+    
+    
+    //Méthode qui effectue l'assèchement de la carte sac de sable.
+    public void assecher(Tuile tuile){
+        tuile.setEtatCase(NORMAL);
+    }
+    
+    
+    
 }
