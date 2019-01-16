2c0dd6c703d20933d0d5aa64e52bbf920fb2c882
chapellr <chapellr@pc-dg-033-10.iut2.upmf-grenoble.fr>
2019-01-16 15:08:38 +0100

    cartesAv

--- Ile interdite/src/ile/interdite/Vue/VueCartesAventurier.java
+++ Ile interdite/src/ile/interdite/Vue/VueCartesAventurier.java
@@ -7,6 +7,9 @@ package ile.interdite.Vue;
 
 import ile.interdite.Modele.Aventuriers.Aventurier;
 import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
@@ -20,6 +23,9 @@ public class VueCartesAventurier {
     private JPanel mainPanel;
     private JLabel labelTitre;
     private JPanel panelCartes;
+    private JButton ok;
+    private JPanel panelFooter;
+    private JPanel panelDroite;
     
     public VueCartesAventurier(Aventurier j){
         this.window = new JFrame("Carte de l'aventurier");
@@ -32,10 +38,33 @@ public class VueCartesAventurier {
         window.add(mainPanel);
         labelTitre = new JLabel("Les cartes de " + j.getPseudo() + " - " + j.getRôle());
         mainPanel.add(labelTitre, BorderLayout.NORTH);
+        int nbligne = j.getCartes().size();
+        if(nbligne % 3 != 0){
+            nbligne = 1+((int)nbligne/3);
+        }
+        
+        panelCartes = new JPanel (new GridLayout(3,nbligne));
         
         for(int i=0; i < j.getCartes().size(); i++){
-            JPanel jp = new JPanel(j.getCartes().get(i));
+            JButton jb = new JButton(new ImageIcon(j.getCartes().get(i).getImage().getImage()));
+            jb.setFocusPainted(false);
+            jb.setContentAreaFilled(false);
+            jb.setBorderPainted(false);
+            panelCartes.add(jb);
         }
+        mainPanel.add(panelCartes, BorderLayout.CENTER);
+        
+        
+        //JButton carteAv = new JButton(new ImageIcon());   AJOUTER L'IMAGE DE L'AVENTURIER
+        panelDroite = new JPanel();
+        mainPanel.add(panelDroite, BorderLayout.EAST);
+        
+        panelFooter = new JPanel(new GridLayout(3, 1));
+        panelFooter.add(new JLabel(""));
+        ok = new JButton("OK");
+        panelFooter.add(ok);
+        panelFooter.add(new JLabel(""));
+        
         
     }
 
